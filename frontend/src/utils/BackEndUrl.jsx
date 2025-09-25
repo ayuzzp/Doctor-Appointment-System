c472cd1fbec2c3f52a61942a39593d1a7e615d37
Ayush <you@example.com>
2025-09-25 13:38:37 +0530

    updated backend url to render

--- frontend/src/utils/BackEndUrl.jsx
+++ frontend/src/utils/BackEndUrl.jsx
@@ -1,5 +1,5 @@
-const BackEndUrl="http://localhost:8000"
+// const BackEndUrl="http://localhost:8000"
+// export default BackEndUrl;
+const BackEndUrl = "https://doctor-appointment-system-2-8l6h.onrender.com";
 export default BackEndUrl;
 
-// const BackEndUrl = import.meta.env.VITE_BACKEND_URL || "https://doctor-appointment-system-2-8l6h.onrender.com";
-// export default BackEndUrl;
